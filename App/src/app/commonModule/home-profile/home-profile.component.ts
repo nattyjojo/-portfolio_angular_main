cf3aa9b9189d4400c244d6514fee469af9623532
nattyjojo <nattyjojo9@gmail.com>
2024-05-25 14:55:37 +0200

    fixing scroll bar

--- App/src/app/commonModule/home-profile/home-profile.component.ts
+++ App/src/app/commonModule/home-profile/home-profile.component.ts
@@ -1,5 +1,6 @@
-import { Component, ViewChild, ElementRef } from '@angular/core';
+import { Component, ViewChild, ElementRef, AfterViewInit } from '@angular/core';
 import { CommonModule } from '@angular/common'; // Import CommonModule
+import { NgScrollbarModule } from '@ngx-scrollbar/core';
 
 @Component({
   selector: 'app-home-profile',
@@ -11,26 +12,37 @@ import { CommonModule } from '@angular/common'; // Import CommonModule
 export class HomeProfileComponent {
   @ViewChild('scrollContainer') scrollContainer!: ElementRef;
   spanPosition: number = 0;
-  scrollLeft(): void {
+
+  ngAfterViewInit() {
+    // You can access targetButton here, after the view has been initialized
+  }
+  scrollTestimonials(): void {
     this.scrollContainer.nativeElement.scrollBy({
       left: this.spanPosition, // Adjust the value as needed
       behavior: 'smooth'
     });
+
   }
 
-  scrollRight(): void {
-    this.scrollContainer.nativeElement.scrollBy({
-      left: this.spanPosition, // Adjust the value as needed
-      behavior: 'smooth'
-    });
+
+  scrollLeftButton(event: MouseEvent){
+    this.spanPosition = -600;
+    this.scrollTestimonials()
+  }
+  scrollRightButton(event: MouseEvent){
+    console.log("clicked")
+    this.spanPosition = 600;
+    this.scrollTestimonials()
   }
   onScrollClick(event : MouseEvent){
     const targetElement = event.currentTarget as HTMLElement;
+    const containerWidth = (event.currentTarget as HTMLElement).offsetWidth;
     const rect = targetElement.getBoundingClientRect();
     const x = event.clientX - rect.left; // X coordinate relative to the div
-    this.spanPosition = x;
-    console.log(this.spanPosition)
-    this.scrollRight()
+    let stringX = x.toString().split('.')[0]
+    console.log({"isthis": stringX})
+    this.spanPosition = Number(stringX);
+    this.scrollTestimonials()
 
 
   }
